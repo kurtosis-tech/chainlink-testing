e58707c88ae0225baebb0d52b840cafd67deb559
galenmarchetti <galenmarchetti@gmail.com>
2021-03-12 15:52:55 -0600

    getting oracle env file

--- testsuite/services_impl/chainlink_oracle/oracle_container_initializer.go
+++ testsuite/services_impl/chainlink_oracle/oracle_container_initializer.go
@@ -7,7 +7,8 @@ import "fmt"
 //								Helper methods
 // ==========================================================================================
 
-func getOracleEnvFile(chainId string, contractAddress string, gethClientIp string, gethClientWsPort string) string {
+func getOracleEnvFile(chainId string, contractAddress string, gethClientIp string, gethClientWsPort string,
+						postgresUsername string, postgresPassword string, postgresServer string, postgresPort string, postgresDatabase string) string {
 	return fmt.Sprintf(`ROOT=/chainlink
 LOG_LEVEL=debug
 ETH_CHAIN_ID=%v
@@ -17,5 +18,8 @@ CHAINLINK_TLS_PORT=0
 SECURE_COOKIES=false
 GAS_UPDATER_ENABLED=true
 ALLOW_ORIGINS=*
-ETH_URL=ws://%v:%v`, chainId, contractAddress, gethClientIp, gethClientWsPort)
+ETH_URL=ws://%v:%v
+DATABASE_URL=postgresql://%v:%v@%v:%v/%v`, chainId, contractAddress,
+	gethClientIp, gethClientWsPort,
+	postgresUsername, postgresPassword, postgresServer, postgresPort, postgresDatabase)
 }
