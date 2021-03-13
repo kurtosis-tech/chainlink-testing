ec6c146ac1b39b1dd6bd669bd90ce0aef8a728af
galenmarchetti <galenmarchetti@gmail.com>
2021-03-13 12:41:15 -0600

    creating oracle container initializer

--- testsuite/services_impl/chainlink_oracle/oracle_container_initializer.go
+++ testsuite/services_impl/chainlink_oracle/oracle_container_initializer.go
@@ -1,20 +1,88 @@
 package chainlink_oracle
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/kurtosis-tech/kurtosis-libs/golang/lib/services"
+	"github.com/kurtosistech/chainlink-testing/testsuite/services_impl/geth"
+	"os"
+)
 
 const (
 	oracleEmail = "user@example.com"
 	oraclePassword = "password"
 	oracleWalletPassword = "walletPassword"
+
+	port = 6688
 )
+
+type ChainlinkOracleInitializer struct {
+	dockerImage string
+}
+
+func NewChainlinkOracleContainerInitializer(dockerImage string) *ChainlinkOracleInitializer {
+	return &ChainlinkOracleInitializer{
+		dockerImage: dockerImage,
+	}
+}
+
+func (initializer ChainlinkOracleInitializer) GetDockerImage() string {
+	return initializer.dockerImage
+}
+
+func (initializer ChainlinkOracleInitializer) GetUsedPorts() map[string]bool {
+	return map[string]bool{
+		fmt.Sprintf("%v/tcp", port): true,
+	}
+}
+
+func (initializer ChainlinkOracleInitializer) GetServiceWrappingFunc() func(ctx *services.ServiceContext) services.Service {
+	return func(ctx *services.ServiceContext) services.Service {
+		return NewChainlinkOracleService(ctx);
+	};
+}
+
+func (initializer ChainlinkOracleInitializer) GetFilesToGenerate() map[string]bool {
+	return map[string]bool{}
+}
+
+func (initializer ChainlinkOracleInitializer) InitializeGeneratedFiles(mountedFiles map[string]*os.File) error {
+	return nil
+}
+
+func (initializer ChainlinkOracleInitializer) GetFilesArtifactMountpoints() map[services.FilesArtifactID]string {
+	return map[services.FilesArtifactID]string{}
+}
+
+func (initializer ChainlinkOracleInitializer) GetTestVolumeMountpoint() string {
+	return geth.TestVolumeMountpoint
+}
+
+func (initializer ChainlinkOracleInitializer) GetStartCommandOverrides(mountedFileFilepaths map[string]string, ipPlaceholder string) (entrypointArgs []string, cmdArgs []string, resultErr error) {
+	entrypointArgs = []string{
+		"/bin/bash",
+		"-c",
+		fmt.Sprintf("%v=%v %v -d %v -h %v -p %v",
+			postgresSuperUserPasswordEnvVar,
+			postgresSuperUserPassword,
+			entrypointScriptPath,
+			databaseName,
+			ipPlaceholder,
+			port),
+	}
+
+	return entrypointArgs, nil, nil
+}
+
+
 
 
 // ==========================================================================================
 //								Helper methods
 // ==========================================================================================
 
-func getOracleEnvFile(chainId string, contractAddress string, gethClientIp string, gethClientWsPort string,
-						postgresUsername string, postgresPassword string, postgresServer string, postgresPort string, postgresDatabase string) string {
+func getOracleEnvFile(chainId string, contractAddress string, gethClientIp string,
+						gethClientWsPort string, postgresUsername string, postgresPassword string,
+						postgresServer string, postgresPort string, postgresDatabase string) string {
 	return fmt.Sprintf(`ROOT=/chainlink
 LOG_LEVEL=debug
 ETH_CHAIN_ID=%v
--- testsuite/services_impl/chainlink_oracle/oracle_service.go
+++ testsuite/services_impl/chainlink_oracle/oracle_service.go
@@ -1,1 +1,22 @@
 package chainlink_oracle
+
+import (
+	"github.com/kurtosis-tech/kurtosis-libs/golang/lib/services"
+)
+
+type ChainlinkOracleService struct {
+	serviceCtx *services.ServiceContext
+}
+
+func NewChainlinkOracleService(serviceCtx *services.ServiceContext) *ChainlinkOracleService {
+	return &ChainlinkOracleService{serviceCtx: serviceCtx}
+}
+
+// ===========================================================================================
+//                              Service interface methods
+// ===========================================================================================
+
+func (postgresService ChainlinkOracleService) IsAvailable() bool {
+	return true
+}
+
